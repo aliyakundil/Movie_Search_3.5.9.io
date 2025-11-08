c4a0a26e3215ec11ff9163d7c36f38c22be0bfbb
aliyakundil <aliya1998n@gmail.com>
2025-11-08 23:18:35 +0600

    add antd

--- src/app/components/MovieCards.tsx
+++ src/app/components/MovieCards.tsx
@@ -1,7 +1,9 @@
 "use client";
-import { Pagination, Input, Select } from "antd";
+import { Pagination, Input, Select, Space, Typography, Card, Tag } from "antd";
 import { useState } from "react";
 import { format } from "date-fns";
+
+const { Text } = Typography;
 
 type Movie = {
   id: number;
@@ -30,16 +32,17 @@ function MovieCards({ movies }: Props) {
 
   return (
     <div className="flex flex-col items-center gap-6 mt-8">
-      <div className="flex justify-center gap-8 mt-6">
-        <div className="flex flex-col items-center cursor-pointer">
-          <span className="text-blue-500 font-medium">Search</span>
+      <Space size="large">
+        <Text className="flex flex-col items-center cursor-pointer">
+          Search
           <div className="w-10 h-[2px] bg-blue-500 mt-1"></div>
-        </div>
-        <div className="flex flex-col items-center cursor-pointer">
-          <span className="text-gray-400 font-medium">Rated</span>
+        </Text>
+
+        <Text className="flex flex-col items-center cursor-pointer">
+          Rated
           <div className="w-10 h-[1px] bg-gray-200 mt-1"></div>
-        </div>
-      </div>
+        </Text>
+      </Space>
 
       {/* Поиск */}
       <Input.Search placeholder="Type to search" size="large" />
@@ -47,40 +50,37 @@ function MovieCards({ movies }: Props) {
       {/* Сетка карточек */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 justify-items-center">
         {currentMovies.map((movie) => (
-          <div
-            key={movie.id}
-            className="movie-card shadow flex gap-6 rounded-xl w-[451px]"
-          >
-            <div className="flex-shrink-0">
-              {movie.poster_path ? (
-                <img
-                  src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-                  alt={movie.title}
-                  className="w-[183px] h-[281px] object-cover"
-                />
-              ) : (
-                <div className="w-[183px] h-[281px] flex items-center justify-center bg-gray-200 text-gray-500">
-                  No Image
-                </div>
-              )}
-            </div>
-            <div className="movie-info flex flex-col justify-start p-3">
-              <div className="movie-title flex items-center justify-between mb-2">
-                <span className="text-xl font-semibold">{movie.title}</span>
-                <div className="flex justify-center items-center text-xs font-normal border-2 w-[30px] h-[30px] border-amber-300 p-1 rounded-full">
-                  {movie.vote_average}
-                </div>
+          <Card hoverable key={movie.id}>
+            <div className="movie-card flex gap-6 rounded-xl w-[451px]">
+              <div className="flex-shrink-0">
+                {movie.poster_path ? (
+                  <img
+                    src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                    alt={movie.title}
+                    className="w-[183px] h-[281px] object-cover"
+                  />
+                ) : (
+                  <div className="w-[183px] h-[281px] flex items-center justify-center bg-gray-200 text-gray-500">
+                    No Image
+                  </div>
+                )}
               </div>
-              <div className="movie-release_date text-gray-600 mb-2">
-                {movie.release_date
-                  ? format(new Date(movie.release_date), "MMMM d, yyyy")
-                  : "No date"}
-              </div>
-              <div className="movie-overview text-sm font-light">
-                {movie.overview}
+              <div className="movie-info flex flex-col justify-start p-3">
+                <div className="movie-title flex items-center justify-between mb-2">
+                  <span className="text-xl font-semibold">{movie.title}</span>
+                  <Tag className="vote">{movie.vote_average}</Tag>
+                </div>
+                <div className="movie-release_date text-gray-600 mb-2">
+                  {movie.release_date
+                    ? format(new Date(movie.release_date), "MMMM d, yyyy")
+                    : "No date"}
+                </div>
+                <div className="movie-overview text-sm font-light">
+                  {movie.overview}
+                </div>
               </div>
             </div>
-          </div>
+          </Card>
         ))}
       </div>
 
